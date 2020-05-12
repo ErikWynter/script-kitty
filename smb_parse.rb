294e9e3bbb4db57372c15bb36c58d8cef042a824
kalba-security <erik.wynter@gmail.com>
2020-05-12 06:26:39 -0400

    Update smb_parse.rb

--- smb_parse.rb
+++ smb_parse.rb
@@ -2,19 +2,25 @@
 def help()
   puts "*" * 70 + "\n\t\t\033[01;32mSMB Parse v1.0\033[00m - \033[01;34mErik Wynter (@wyntererik)\033[00m"
   puts "*" * 70
-  puts "\n  Usage: #{$0} [file1] [file2] [file3] ... [filex]"
+  puts "\nEasily parse the results of Nmap's 'smb-enum-shares' NSE script."
+  puts "Compatible with .nmap files or copy-pasted 'smb-enum-shares' output."
+  puts "\nUsage: #{$0} [file1] [file2] [file3] ... [filex]"
    puts"  -h\t\tDisplay this menu and exit."
   puts "  -o [scheme]\tNaming scheme for the output file(s). For multiple\n\t\tinput files, the name of each input file is appended\n\t\tto the scheme for the respective outpute file.\n\t\tDefault scheme: 'parsed_[inputfile]'"
   puts "\n  Example: #{$0} nmap_smb_results1.txt nmap_smb_results2.txt"
-  puts "  Example: #{$0} smb_enum_results.txt -o smb_enum_parsed.txt"
+  puts "  Example: #{$0} scan1.txt -o results"
   exit
 end
 
 warning = "[" + "\033[01;33m" + "!" + "\033[00m" + "]"
+info = "[" + "\033[01;34m" + "*" + "\033[00m" + "]"
+success = "[" + "\033[01;32m" + "+" + "\033[00m" + "]"
+error = "[" + "\033[01;31m" + "-" + "\033[00m" + "]"
 
 if ARGV.length() == 0
-  puts "#{warning} Please provide a file to parse. Quitting!"
-  exit
+  puts "#{warning} Please provide at least one file to parse."
+  puts "#{info} Loading help menu:"
+  help()
 end
 
 if ARGV.include? '-h'
@@ -37,68 +43,71 @@ else
 end
 
 args.each do |file| 
-  results = {}
-  null_session_ct = 0
-  hosts_info = File.open(file).read.split("report for ")
-  hosts_info.shift
-  hosts_info.each do |data|
-    if data.include? "Anonymous access: READ"
-      ip = data.split("\n")[0]
-      results[ip] = []
-      shares = data.split("\\\\")
-      shares.shift
-      shares.each do |s|
-        lines = s.split("\n")
-        share = lines[0].strip().gsub(":","")
-        lines.each do |l|
-          if l.include? 'Anonymous access: R'
-            anon_access = l.split(": ")[1]
-            unless l.split(": ")[1] == nil
-              results[ip].append([share,anon_access])
-              null_session_ct += 1
+  begin
+    results = {}
+    null_session_ct = 0
+    hosts_info = File.open(file).read.split("report for ")
+    hosts_info.shift
+    hosts_info.each do |data|
+      if data.include? "Anonymous access: READ"
+        ip = data.split("\n")[0]
+        results[ip] = []
+        shares = data.split("\\\\")
+        shares.shift
+        shares.each do |s|
+          lines = s.split("\n")
+          share = lines[0].strip().gsub(":","")
+          lines.each do |l|
+            if l.include? 'Anonymous access: R'
+              anon_access = l.split(": ")[1]
+              unless l.split(": ")[1] == nil
+                results[ip].append([share,anon_access])
+                null_session_ct += 1
+              end
             end
           end
         end
       end
     end
-  end
-  if results.length() == 0
-    failure = "[" + "\033[01;31m" + "-" + "\033[00m" + "]"
-    puts "#{failure} #{file}: Did not find shares allowing null session authentication."
-  else
-    if file.include? "/"
-      file = file.split("/")
-      file = file[file.length() -1]
-    end
-    if out_scheme
-      if args.length() > 1
-        out_file = out_scheme + "_" + file
-      else
-        out_file = out_scheme
-      end
+    if results.length() == 0
+      failure = "[" + "\033[01;31m" + "-" + "\033[00m" + "]"
+      puts "#{failure} #{file}: Did not find shares allowing null session authentication."
     else
-      out_file = "parsed_" + file
-    end
-    #write to out_file
-    success = "[" + "\033[01;32m" + "+" + "\033[00m" + "]"
-    info = "[" + "\033[01;34m" + "*" + "\033[00m" + "]"
-    puts "#{success} #{file}: #{null_session_ct} shares accross #{results.length()} hosts allow null session authentication."
-    puts "#{info} Writing results to '#{out_file}'.\n"
-    puts "Affected hosts:"
-    hosts = results.keys
-    hosts = hosts.each_slice(5).to_a
-    hosts.each { |ip| puts ip.join("   ")}
-    File.open(out_file, "w") {
-      |f| results.each do |ip|
-        puts "-" *62 + "\n|IP: #{ip[0]}"
-        f.write("-" *62 + "\n|IP: #{ip[0]}\n")
-        ip[1].each {
-          |i| puts "|-Share: #{i[0]}\t- Anonymous access: #{i[1]}"
-          f.write("|-Share: #{i[0]}\t- Anonymous access: #{i[1]}\n")
-        }
+      if file.include? "/"
+        file = file.split("/")
+        file = file[file.length() -1]
+      end
+      if out_scheme
+        if args.length() > 1
+          out_file = out_scheme + "_" + file
+        else
+          out_file = out_scheme
         end
-        puts "-" *62
-        f.write("-" *62 + "\n")
-    }
+      else
+        out_file = "parsed_" + file
+      end
+      puts "#{success} #{file}: #{null_session_ct} shares accross #{results.length()} hosts allow null session authentication."
+      puts "#{info} Writing results to '#{out_file}'.\n"
+      puts "Affected hosts:"
+      hosts = results.keys
+      hosts = hosts.each_slice(5).to_a
+      hosts.each { |ip| puts ip.join("   ")}
+      File.open(out_file, "w") {
+        |f| results.each do |ip|
+          puts "-" *62 + "\n|IP: #{ip[0]}"
+          f.write("-" *62 + "\n|IP: #{ip[0]}\n")
+          ip[1].each {
+            |i| puts "|-Share: #{i[0]}\t- Anonymous access: #{i[1]}"
+            f.write("|-Share: #{i[0]}\t- Anonymous access: #{i[1]}\n")
+          }
+          end
+          puts "-" *62
+          f.write("-" *62 + "\n")
+      }
+    end
+  rescue
+    puts "#{error} Unable to parse '#{file}'."
+    puts "#{warning} Please provide only .nmap files or files containing copy-pasted Nmap output.\n\n"
+    next
   end
 end
