2d1ee91b7da059ff1dca6cac9e0af6793f5e87ad
kalba-security <erik.wynter@gmail.com>
2020-06-04 15:17:44 -0400

    Fix speed issue psp.rb

--- psp/psp.rb
+++ psp/psp.rb
@@ -68,9 +68,8 @@ class Parser
     @pr.print_info("Treating `#{@file}` as a .nmap file.")
     hosts_info = @contents.split("report for ") #split file contents into chunkcs for each host
     hosts_info.shift #remove nmap initiation info
-    
+    ct = 0
     port_hash = {}
-    ip_hash = {}
     ip_arr = []
     hosts_info.each do |data|  
       lines = data.split("\n")
@@ -80,21 +79,21 @@ class Parser
       lines.each do |l|
         if l.include?("open") && !l.include?("filtered")
           port = l.split("/")[0]
+          ct += 1
           if port_hash.include?(port)
             port_hash[port].append(ip)
           else
             port_hash[port] = [ip]
           end
-          if ip_hash.include?(ip)
-            ip_hash[ip].append(port)
-          else
-            ip_hash[ip] = [port]
-          end
         end
       end
     end
+    if ct > 0 && ct % 100000 == 0
+        @pr.print_info("Read #{ct} lines so far...")
+      end
+    ip_arr = ip_arr.uniq
     if port_hash.length > 0
-      return ip_arr, port_hash, ip_hash
+      return ip_arr, port_hash
     else
       return [ip_arr]
     end
@@ -104,68 +103,63 @@ class Parser
     @pr.print_info("Treating `#{@file}` as a .gnmap file.")
     hosts_info = @contents.split("Host: ") #split file contents into chunkcs for each host
     hosts_info.shift #remove gnmap initiation info
-    
+    ct = 0
     port_hash = {}
-    ip_hash = {}
     ip_arr = []
     hosts_info.each do |data|
-      ip = data.split(" ")[0]
-      unless ip_arr.include?(ip)
-        ip_arr.append(ip)
-      end
-      if data.include?("open")
+      if data.include?("Up")
+        ip = data.split(" ")[0]
+      elsif data.include?("open")
+        ip = data.split(" ")[0]
         port_info = data.split("Ports: ")[1].split(" ")
         port_info.each do |p|
           if p.include?("open") && !p.include?("filtered")
             port = p.split("/")[0]
+            ct += 1
             if port_hash.include?(port)
               port_hash[port].append(ip)
             else
               port_hash[port] = [ip]
             end
-            if ip_hash.include?(ip)
-              ip_hash[ip].append(port)
-            else
-              ip_hash[ip] = [port]
-            end
           end
         end
       end
+      if ip
+        ip_arr.append(ip)
+      end
+      if ct > 0 && ct % 100000 == 0
+        @pr.print_info("Read #{ct} lines so far...")
+      end
     end
+    ip_arr = ip_arr.uniq
     if port_hash.length > 0
-      return ip_arr, port_hash, ip_hash
+      return ip_arr, port_hash
     else
       return [ip_arr]
     end
   end
 
-  def write_files(results)
+  def write_files(results,directory)
     ip_arr = results[0]
     port_hash = results[1]
     ip_hash = results[2]
     file_base = @file.sub(/\.[^.]+\z/, '')
+    file_base = file_base.split("/")
+    file_base = file_base[file_base.length-1]
     
     if port_hash
       @pr.print_good("Creating text file(s) with live hosts for the #{port_hash.length} open port(s) found.")
       port_hash.each do |port,ips|
-        port_file = "#{port}.txt"
+        port_file = "#{directory}#{port}.txt"
         File.open(port_file, "w") {
           |f| ips.each do |ip|
             f.write("#{ip}\n")
           end
         }
       end
-  
-      ip_port_file = "#{file_base}.ports"
-      @pr.print_good("Writing #{ip_hash.length} IPs and their respective open ports to `#{ip_port_file}`.")
-      File.open(ip_port_file, "w") {
-        |g| ip_hash.each do |ip,ports|
-          g.write("#{ip} - #{ports.join(" ")}\n")  
-        end
-      }
     end
 
-    ip_file = "#{file_base}.ips"
+    ip_file = "#{directory}#{file_base}.ips"
     @pr.print_good("Writing all #{ip_arr.length} IPs in `#{@file}` to `#{ip_file}`.")
     File.open(ip_file, "w") {
       |h| ip_arr.each do |ip|
@@ -209,34 +203,34 @@ if $0 == __FILE__
   end
   directory += "/" if directory[-1] != "/"
 
-  pr.print_info("Getting ready to parse some files...")
-  ARGV.each do |file| 
-    unless File.file?(file)
-      pr.print_warning("Skipping `#{file}` because it does not exist. Please check for typos.")
-      next
-    end
+  pr.print_info("Getting ready...")
+  file = ARGV[0]
+  unless File.file?(file)
+    pr.print_failure("Can't parse `#{file}` because it does not exist. Please check for typos.")
+    pr.print_warning("Quitting!")
+    exit
+  end
 
-    begin
-      contents = File.open(file).read
-      parse = Parser.new(file,contents,directory)
-      if contents.include?("report for")
-        results = parse.nmap
-      elsif contents.include?("Host:")
-        results = parse.gnmap
-      else
-        pr.print_warning("Skipping `#{file}` because it is not a compatible filetype. Please provide .nmap and .gnmap files only.")
-        next
-      end
-      unless results.length == 3
-        pr.print_failure("#{file} does not contain any hosts with open ports.")
-      end
-      parse.write_files(results)
-    rescue
-      raise #TODO: remove after testing
-      pr.print_failure("An error occurred while trying to parse `#{file}`. Skipping...")
-      pr.print_warning("Please provide only .nmap and .gnmap files.\n\n")
-      next
+  begin
+    contents = File.open(file).read
+    parse = Parser.new(file,contents,directory)
+    if contents.include?("report for")
+      results = parse.nmap
+    elsif contents.include?("Host:")
+      results = parse.gnmap
+    else
+      pr.print_failure("Can't parse `#{file}` because it is not a compatible filetype. Please provide a .nmap or .gnmap file.")
+      pr.print_warning("Quitting!")
+      exit
     end
+    unless results.length == 2
+      pr.print_failure("#{file} does not contain any hosts with open ports.")
+    end
+    parse.write_files(results,directory)
+  rescue
+    pr.print_failure("The following error occurred while trying to parse `#{file}`.")
+    pr.print_warning("Please provide only .nmap and .gnmap files.\n\n")
+    raise
   end
   pr.print_info("No tasks left. Time for a well-deserved nap.")
 end
